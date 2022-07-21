801c41e005399524e94ec05b6eb33edcfd85d197
Administrator <researchequity10@gmail.com>
2022-07-21 17:36:30 +0530

    in latest base file only latest qauter we will take

--- bse_shareholdings_manual.py
+++ bse_shareholdings_manual.py
@@ -23,11 +23,15 @@ data_df = daily_data_df
 
 daily_data_df = pd.read_csv(PROCESSED_DIR + "\\latest_detail_shareholdings.csv")
 
-daily_data_df['Date'] = pd.to_datetime(daily_data_df['Date'])
+# daily_data_df['Date'] = pd.to_datetime(daily_data_df['Date'])
+daily_data_df['Quarter'] = pd.to_datetime(daily_data_df['Quarter'], format='%b-%y')
+daily_data_df = daily_data_df[daily_data_df.Quarter == (daily_data_df['Quarter']).max()]
+daily_data_df['Quarter'] = daily_data_df['Quarter'].dt.strftime('%b-%y')
 daily_data_df.rename({'Scrip_code': 'Script_code'}, axis=1, inplace=True)
 
-# data_df.rename({'Total nos. shares held': 'Share', 'Nos. of shareholders': 'Holder',
-#                 'Nationality': 'Type', 'Category of shareholder': 'Detail'}, axis=1, inplace=True)
+data_df.rename({'Total nos. shares held': 'Share', 'Nos. of shareholders': 'Holder',
+                    'Nationality': 'Type', 'Category of shareholder': 'Detail',
+                    'Shareholding as a % of total no. of shares (calculated as per SCRR, 1957)As a % of (A+B+C2)': 'Sharehold_perct'}, axis=1, inplace=True)
 
 daily_base_df = pd.read_csv(PROCESSED_DIR + "\\Historical_base_shareholding_pattern.csv")
 # Historical_base_df = pd.read_csv(input_filepath + "Historical_base_shareholding_pattern.csv")
@@ -41,7 +45,7 @@ data_base_df.rename({'Scrip_code': 'Script_code', 'Total no. shares held': 'Shar
                     axis=1, inplace=True)
 
 # Input and Output Files
-FII = 0
+FII = 1
 SuperStar = 0
 
 if FII == 1:
@@ -64,8 +68,8 @@ if ReportType == 'FII':
 elif ReportType == 'Superstar':
     SuperStar = 1
 
-Date = (wb.sheets("Input").range('C6').options(index=False).value)
-quarters = (wb.sheets("Input").range('C8').options(index=False).value).split(',')
+Date = wb.sheets("Input").range('C6').options(index=False).value
+quarters = wb.sheets("Input").range('C8').options(index=False).value.split(',')
 script_code = int(wb.sheets("Input").range('C4').options(index=False).value)
 q_num = 1
 quarter_qx = []
@@ -102,14 +106,13 @@ today_company_result.drop(0, axis=1, inplace=True)
 wb_input.range("G2").options(index=False).value = today_company_result
 
 quarter_string =","
-#drop if nan value exist
 data_base_df.dropna(subset = ["Quarter"], inplace=True)
 print(data_base_df['Quarter'].unique())
 wb_input.range("C13").options(index=False).value = quarter_string.join(data_base_df['Quarter'].unique())
 
 
 if SuperStar == 1:
-    superstar_name_df = pd.read_csv(r"D:\Program\python_ankit\ProjectDir\data" +'\\superstar_name.csv')
+    superstar_name_df = pd.read_csv(r"D:\Program\python_ankit\ProjectDir\data" + '\\superstar_name.csv')
     our_superstar = superstar_name_df['Our_SS']
     print(script_code_today)
 
@@ -117,29 +120,23 @@ if SuperStar == 1:
     data_df['Company'] = data_df['Script_code']
 
     data_df = data_df[data_df['Category of shareholder'].isin(our_superstar)]
-    # data_df = data_df[data_df['Detail'].str.contains("JHUNJHUNWALA RAKESH|Jhunjhunwala Rakesh", na=False)]
     data_base_df = data_base_df[data_base_df['Script_code'].isin(script_code_today)]
     data_base_df['Company'] = data_base_df['Script_code']
     data_base_df = data_base_df[data_base_df['Category'] == '(B) Public']
 elif FII == 1:
     print(script_code_today)
-    Category = (wb.sheets("Input").range('C11').options(index=False).value)
-    #data_df = data_df[data_df['Script_code'].isin(script_code_today)]
+    Category = wb.sheets("Input").range('C11').options(index=False).value
     data_df['Company'] = data_df['Script_code']
-    data_df = data_df[data_df['Category'] == Category] #'Foreign Portfolio Investors'
-    #data_base_df = data_base_df[data_base_df['Script_code'].isin(script_code_today)]
+    data_df = data_df[data_df['Category'] == Category]  # 'Foreign Portfolio Investors'
     data_base_df['Company'] = data_base_df['Script_code']
-    #data_base_df = data_base_df[data_base_df['Category'] == '(B) Public'] #(A) Promoter & Promoter Group
-    data_base_df = data_base_df[data_base_df['Category'] == '(A) Promoter & Promoter Group']
+    filt = str(wb.sheets("Input").range('C12').options(index=False).value)
+    data_base_df = data_base_df[data_base_df['Category'] == filt]   # (A) Promoter & Promoter Group
+    # data_base_df = data_base_df[data_base_df['Category'] == '(A) Promoter & Promoter Group']
 
 else:
     data_df = data_df[data_df['Script_code'].isin(script_code_today)]
     data_base_df = data_base_df[data_base_df['Script_code'].isin(script_code_today)]
     data_base_df['Company'] = data_base_df['Script_code']
-    # each quater name are different so just pick one
-    # company = data_base_df['Company'].unique()[0]
-    # data_df['Company'] = company
-    # data_base_df['Company'] = company
 
 # Hard Code Input Quarter
 data_df = data_df[data_df['Quarter'].isin(quarters)]
@@ -161,36 +158,36 @@ wb_base.range("A1").options().value = data_base_df
 
 # Get Header Data
 if SuperStar != 1:
-    data_header_df = data_df[data_df['Category'] == data_df['Category of shareholder']]
+    data_header_df = data_df[data_df['Category'] == data_df['Detail']]
     data_header_df.drop('Company', axis=1, inplace=True)
     data_header_df = pd.merge(today_company_result, data_header_df, on=['Script_code'])
-    data_header_df.drop_duplicates(subset=["Script_code", "Company", "Quarter", "Nationality", "Category", "Header"], keep=False,inplace=True)
-
-    #data_header_df.to_csv(input_filepath + '\\stage_fii_comp.csv')
-    data_header_df = data_header_df.pivot(index=['Script_code', 'Company', 'Nationality', 'Category'], columns='Quarter',values=['Total nos. shares held', 'Nos. of shareholders'])
+    data_header_df.drop_duplicates(
+        subset=["Script_code", "Company", "Quarter", "Type", "Detail", "Holder", "Header", "Share"], keep=False,
+        inplace=True)
+    data_header_df = data_header_df.pivot(index=['Script_code', 'Company', 'Type', 'Category'], columns='Quarter',
+                                          values=['Sharehold_perct', 'Share'])
     data_header_df.columns = list(map("_".join, data_header_df.columns))
     data_header_df.to_csv(RAW_DIR + '\\stage_fii_comp.csv')
     data_header_csv = pd.read_csv(RAW_DIR + '\\stage_fii_comp.csv')
-    print(data_header_csv.head())
 
     for i in range(len(quarters)-1):
         print(quarters)
         i=i+1
         print(i)
-        data_header_csv['diff_share_%_q'+str(i)+'q'+str(i+1)] = round((data_header_csv['Total nos. shares held_q'+str(i)] - data_header_csv['Total nos. shares held_q'+str(i+1)])* 100 / data_header_csv['Total nos. shares held_q'+str(i+1)],2)
-        #data_header_csv = data_header_csv[data_header_csv['Share_q1'] != data_header_csv['Share_q3']]
-
+        data_header_csv['diff_share_%_q' + str(i) + 'q' + str(i + 1)] = round(
+            (data_header_csv['Sharehold_perct_q' + str(i)] - data_header_csv['Sharehold_perct_q' + str(i + 1)]) * 100 /
+            data_header_csv['Sharehold_perct_q' + str(i + 1)], 2)
 
     wb_header.range("A1").options(index=None).value = data_header_csv
 
 # Get Detail Data
-data_detail_df = data_df[data_df['Category'] != data_df['Category of shareholder']]
+data_detail_df = data_df[data_df['Category'] != data_df['Detail']]
 data_detail_df.drop('Company', axis=1, inplace=True)
 data_detail_df = pd.merge(today_company_result, data_detail_df, on=['Script_code'])
-data_detail_df.drop_duplicates(subset=["Script_code", "Company", "Quarter", "Nationality", "Category of shareholder"],keep=False, inplace=True)
-data_detail_df = data_detail_df.pivot(index=['Script_code', 'Company', 'Nationality', 'Category', 'Category of shareholder'], columns='Quarter', values=['Total nos. shares held', 'Nos. of shareholders'])
+data_detail_df.drop_duplicates(subset=["Script_code", "Company", "Type", "Detail", "Quarter"], keep=False, inplace=True)
+data_detail_df = data_detail_df.pivot(index=['Script_code', 'Company', 'Type', 'Category', 'Detail'], columns='Quarter',
+                                      values=['Sharehold_perct', 'Share'])
 data_detail_df.columns = list(map("_".join, data_detail_df.columns))
-
 # print to sheet
 wb_detail.range("A1").options().value = data_detail_df
 
@@ -198,5 +195,4 @@ wb_detail.range("A1").options().value = data_detail_df
 if FII == 0:
     file_selected.to_csv("D:\\Program\\Data\\Bse_ShareHolding_Files\\" + "_" + str(script_code) + ".check")
 
-# data_header_df.to_csv("D:\\Program\\Data\\data_header_df.csv")
-# data_detail_df.to_csv("D:\\Program\\Data\\data_detail_df.csv")
+
